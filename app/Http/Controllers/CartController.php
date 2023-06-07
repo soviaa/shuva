38bd4c08e8049439655772014e924e63bc846120
soviaa <soviamdr@gmail.com>
2023-06-07 15:33:14 +0545

    cart and checkout works

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use App\Models\User;
 use App\Models\Cart;
+use App\Models\Order;
+use App\Models\OrderItem;
 
 use Illuminate\Support\Facades\Auth;
 
@@ -18,17 +20,23 @@ class CartController extends Controller
     public function showCart()
     {
         if (!auth()->check()) {
-            return redirect()->route('login')->with('error', 'Please log in to view your cart.');
+            return redirect()->route('userlogin')->with('error', 'Please log in to view your cart.');
         }
     
         $cartItems = Cart::where('user_id', auth()->user()->id)->get();
+        $totalAmount = $cartItems->sum(function ($item) {
+            return $item->quantity * $item->price;
+        });
     
-        return view('user.usercart', compact('cartItems'));
+        return view('user.usercart', compact('cartItems', 'totalAmount'));
     }
-    
+
     public function addToCart(Request $request)
     {
-        
+        if (!auth()->check()) {
+            return redirect()->route('userlogin')->with('error', 'Please log in to add products to your cart.');
+        }
+    
         $productId = $request->input('product_id');
         $quantity = $request->input('quantity');
     
@@ -44,19 +52,100 @@ class CartController extends Controller
         $total = $product->price * $quantity;
     
         // Create a new cart item
-  
-
-
         $cartItem = new Cart();
-        
+    
         $cartItem->user_id = $user->id;
         $cartItem->product_id = $product->id;
-        $cartItem->quantity =$quantity;
+        $cartItem->quantity = $quantity;
         $cartItem->price = $product->price;
         $cartItem->total = $total;
         $cartItem->save();
     
         return redirect()->back()->with('success', 'Product added to cart.');
     }
+    public function updateCartQuantity(Request $request, $id)
+{
+    $quantity = $request->input('quantity');
     
-}    
+    $cartItem = Cart::find($id);
+    
+    if (!$cartItem) {
+        return redirect()->back()->withErrors('Cart item not found.');
+    }
+    
+    $cartItem->quantity = $quantity;
+    $cartItem->total = $cartItem->price * $quantity;
+    $cartItem->save();
+    
+    return redirect()->back()->with('success', 'Cart item quantity updated.');
+}
+public function deleteCartItem($id)
+{
+    $cartItem = Cart::find($id);
+    
+    if (!$cartItem) {
+        return redirect()->back()->withErrors('Cart item not found.');
+    }
+    
+    $cartItem->delete();
+    
+    return redirect()->back()->with('success', 'Cart item deleted.');
+}
+public function processCheckout(Request $request)
+{
+    // Get the authenticated user
+    $user = Auth::user();
+
+    // Get the cart items for the user
+    $cartItems = Cart::where('user_id', $user->id)->get();
+
+    // Calculate the total amount
+    $totalAmount = $cartItems->sum(function ($item) {
+        return $item->quantity * $item->price;
+    });
+
+    // Create a new order
+    $order = new Order();
+    $order->user_id = $user->id;
+    $order->total_amount = $totalAmount;
+    $order->status = 'pending'; // Default status is pending
+    $order->save();
+
+    // Move cart items to order items
+    foreach ($cartItems as $cartItem) {
+        $order->items()->create([
+            'product_id' => $cartItem->product_id,
+            'quantity' => $cartItem->quantity,
+            'price' => $cartItem->price,
+            'total' => $cartItem->quantity * $cartItem->price,
+        ]);
+    }
+
+    // Clear the cart for the user
+    $cartItems->each(function ($cartItem) {
+        $cartItem->delete();
+    });
+
+    // Generate the QR code URL (you need to customize this according to your QR code generation logic)
+    $qrCodeUrl = 'https://example.com/qrcode?order_id=' . $order->id;
+
+    // Redirect to the checkout success page with the QR code URL
+    return redirect()->route('checkout.success', ['qrCodeUrl' => $qrCodeUrl])->with('success', 'Order placed successfully.');
+}
+
+
+public function checkoutSuccess()
+{
+    // Get the authenticated user
+    $user = Auth::user();
+
+    // Get the latest order for the user
+    $order = Order::where('user_id', $user->id)->latest()->first();
+
+  
+
+    return view('user.checkout');
+
+}
+}
+
--- resources/views/product/carpets.blade.php
+++ resources/views/product/carpets.blade.php
@@ -17,13 +17,13 @@
                 <span>Rs.{{$product->price}}</span>
               </div>
               <div class="pro_cart">
-                <button class="popup-btn" data-image="/images/products/{{$product->image}}" data-name="{{$product->product_name}}" data-price="{{$product->price}}">
+              <button class="popup-btn" data-image="/images/products/{{$product->image}}" data-name="{{$product->product_name}}" data-price="{{$product->price}}" data-product="{{$product->id}}">
                   <i class="ri-shopping-bag-line"></i>
                   <span>Shop Now</span>
                 </button>
               </div>
             </div>
-          </div>
+          </div> 
         @endif
       @endforeach
     </div>
@@ -40,33 +40,42 @@
         <div class="popup-info">
           <h2></h2>
           <div class="popup-price"></div>
-          <div class="popup_cart">
-            <a href="#" style="color:white">
-              <i class="ri-shopping-cart-line"></i>
-              <span>Add to cart</span>
-            </a>
-          </div>
+          <form class="add-to-cart-form" action="{{ route('addtocart')}}" method="POST">
+            @csrf
+            <input type="hidden" name="product_id" id="product_id">
+            <label for="quantity">Quantity:</label>
+            <input type="number" name="quantity" id="quantity" value="1" min="1">
+            <button type="submit" class="add-to-cart-btn">Add to cart</button>
+          </form>
+  <div id="cart-popup-message" class="cart-popup-message hide">
+  Product added to cart
+</div>
         </div>
       </div>
     </div>
   </div>
 
   <script>
+    // description of product
     const popupBtns = document.querySelectorAll('.popup-btn');
     const popupView = document.querySelector('.popup_view');
     const closeBtn = document.querySelector('.close-btn-popup');
     const popupImg = document.querySelector('.popup-img img');
     const popupInfo = document.querySelector('.popup-info');
+    const cartForm = document.querySelector('.add-to-cart-form');
+    const cartMessage = document.getElementById('cart-popup-message');
 
     popupBtns.forEach(btn => {
       btn.addEventListener('click', () => {
         const imageSrc = btn.dataset.image;
         const productName = btn.dataset.name;
         const productPrice = btn.dataset.price;
+        const productId = btn.dataset.product; // Add this line
 
         popupImg.src = imageSrc;
         popupInfo.querySelector('h2').textContent = productName;
         popupInfo.querySelector('.popup-price').textContent = `Rs.${productPrice}`;
+        document.getElementById('product_id').value = productId; // Add this line
 
         popupView.classList.add('active');
       });
@@ -75,5 +84,9 @@
     closeBtn.addEventListener('click', () => {
       popupView.classList.remove('active');
     });
+
+
+
+
   </script>
 @endsection
